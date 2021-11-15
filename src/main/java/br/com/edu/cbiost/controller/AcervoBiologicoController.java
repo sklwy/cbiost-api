ff9ec0e127fe50e7251e8599c1d90a63bc88ab6c
Tiago Boaventura Miranda <78670580+sklwy@users.noreply.github.com>
2021-11-15 17:03:36 -0300

    Update AcervoBiologicoController.java
    Added annotation @CrossOrigin

--- src/main/java/br/com/edu/cbiost/controller/AcervoBiologicoController.java
+++ src/main/java/br/com/edu/cbiost/controller/AcervoBiologicoController.java
@@ -3,6 +3,7 @@ package br.com.edu.cbiost.controller;
 import br.com.edu.cbiost.model.AcervoBiologico;
 import br.com.edu.cbiost.service.AcervoBiologicoServiceImpl;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/api/acervo-biologico")
+@CrossOrigin
 public class AcervoBiologicoController {
 
     private final AcervoBiologicoServiceImpl acervoBiologicoService;
